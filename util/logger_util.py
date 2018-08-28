32a3894062153e29ace37fb6733506fbbf20eb44
1065120559@qq.com <@miczdl37wznb>
2018-08-28 10:44:13 +0800

    logger

--- util/logger_util.py
+++ util/logger_util.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2017/12/21 
+# @Time    : 2018/12/21
 # @Author  : ErichLee ErichLee@qq.com
 # @File    : logger_util.py
 # @Comment : 打印工具
@@ -7,14 +7,36 @@
 import logging
 
 
+def print_new_line(msg=''):
+    if msg:
+        print(msg)
+        logging.info(msg)
+    else:
+        print()
+
+
+def print_no_line(msg=''):
+    if msg:
+        # print(msg, end='')  # py3版本
+        print msg,  # py2版本
+        logging.info(msg)
+
+
+def __get_meg(args):
+    msg = [str(each) for each in args if each]
+    return ' '.join(msg)
+
+
+def infos_line(*args):
+    if args:
+        msg = __get_meg(args)
+        print_no_line(msg)
+
+
 def infos(*args):
     if args:
-        str_list = []
-        for each in args:
-            str_list.append(str(each))
-        msg = ' '.join(str_list)
-        print msg
-        logging.info(args)
+        msg = __get_meg(args)
+        print_new_line(msg)
 
 
 def ljinfos(lmgs, *args):
@@ -23,14 +45,15 @@ def ljinfos(lmgs, *args):
     :param args: 变量参数依次打印
     """
     if lmgs:
-        print '[{}]:'.format(lmgs.ljust(30)),
+        print_no_line('[{}]:'.format(lmgs.ljust(30)))
     else:
-        print '[{}]'.format("".ljust(30)),
+        print_no_line('[{}]'.format("".ljust(30)))
 
-    if any(args):
+    if args:
         for each in args:
-            print each,
-    print
+            print_no_line(each)
+
+    print_new_line()
 
 
 def cinfos(lmgs, *args):
@@ -39,25 +62,31 @@ def cinfos(lmgs, *args):
     :param args: 变量参数依次打印
     """
     if lmgs:
-        print '[{}]:'.format(lmgs.center(30)),
+        print_no_line('[{}]:'.format(lmgs.center(30)))
     else:
-        print '[{}]'.format("".center(30)),
+        print_no_line('[{}]'.format("".center(30)))
 
-    if any(args):
+    if args:
         for each in args:
-            print each,
-    print
+            print_no_line(each)
 
-
-def println():
-    print '================================'
+    print_new_line()
 
 
 def errors(*args):
     if args:
-        str_list = []
-        for each in args:
-            str_list.append(str(each))
-        msg = ' '.join(str_list)
-        print '[ERROR]: {}'.format(msg)
-        logging.info(args)
+        msg = __get_meg(args)
+        print_new_line('[ERROR]: {}'.format(msg))
+
+
+def println():
+    print_new_line('================================')
+
+# infos()
+# errors()
+# infos('1', None)
+# infos_line('1', 2, 3, '4')
+# infos('next line')
+# ljinfos('title', '1', 2, 3, None, '4', None)
+# cinfos('title', '1', 2, 3, '4')
+# errors('error', 'msssag', 'exception')
