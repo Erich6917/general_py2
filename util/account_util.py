04ba94e0d956bf60ad546aa7aa9f5f766f55a649
liyuan <ErichLee@qq.com>
2021-01-05 14:21:16 +0800

    py2 工具类整理

---
+++ util/account_util.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021-1-5 
+# @Author  : ErichLee ErichLee@qq.com
+# @File    : account_util.py
+# @Comment : 
+#            
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+SECRET_EMAIL_ACCOUNT_187 = ''
+SECRET_EMAIL_PASSWORD_187 = ''
+
+
+SECRET_EMAIL_ACCOUNT_NEWBEE = ''
+
+
